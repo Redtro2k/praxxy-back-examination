94ca6abc08bf96dec61cce8db0706064a1c88fe9
Blu369 <llacuna.darwin24@gmail.com>
2023-06-28 13:23:39 +0800

    complete functiona category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Inertia\Inertia;
 use App\Models\Category;
 use App\Http\Requests\CategoryRequest;
+use Carbon\Carbon;
+use App\Models\Product;
 
 class CategoryController extends Controller
 {
@@ -15,12 +17,21 @@ class CategoryController extends Controller
         $categories = $this->category->all()->map(fn($m) => [
             'id' => $m->id,
             'title' => $m->title,
-            'items' => $m->product()->count()
+            'items' => $m->product()->whereDate('created_at', '>', Carbon::now()->subDay())->count(),
         ]);
         return Inertia::render("Category/CategoryIndex", [
             'categories' => $categories,
-            'category' => !!$id ? $this->category->findOrFail($id) : null
+            'category' => !!$id ? $this->category->findOrFail($id) : null,
+            'new_items' => $this->category->whereDate('created_at', '>', Carbon::now()->subHour())->count()
         ]);
+    }
+    public function store(CategoryRequest $request){
+        if(!$request->validated()){
+            return abort(500);
+        }else{
+            $this->category->create(['title' => $request->title]);
+            return redirect()->route('category.index')->with('success', 'Successfully Create New Categroy');
+        }
     }
     public function update(CategoryRequest $request, $id){
         if($request->validated()){
@@ -32,5 +43,13 @@ class CategoryController extends Controller
         }else{
             return abourt(419);
         }
+    }
+    public function destroy($id){
+        $get_product = Product::where('category_id', $id)->get();
+        if($get_product->count() != 0){
+            $get_product->each->delete();
+        }
+        $this->category->find($id)->delete();
+        return redirect()->back()->with('success', 'successfully deleted One Category');
     }
 }
--- resources/js/Pages/Category/CategoryIndex.vue
+++ resources/js/Pages/Category/CategoryIndex.vue
@@ -1,4 +1,5 @@
 <template>
+<!-- for viewing category -->
 <TransitionRoot as="template" :show="open">
     <Dialog as="div" class="relative z-10" @close="open = false">
         <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
@@ -11,7 +12,8 @@
                 <form @submit.prevent="submit">
                 <div>
                 <div class="mt-2 sm:mt-4">
-                    <DialogTitle as="h3" class="text-lg font-medium leading-6 text-gray-900 dark:text-gray-200">Edit Category</DialogTitle>
+                    <DialogTitle as="h3" class="text-lg font-bold leading-6 text-gray-900 dark:text-gray-200">Edit Category</DialogTitle>
+                    <p class="text-gray-400 pb-2 text-xs">Create a new category to your product.</p>
                     <div class="mt-2">
                             <div>
                                 <InputLabel for="Title" value="Title" />
@@ -30,9 +32,11 @@
                 </div>
                 </div>
                 <div class="mt-5 sm:mt-6 flex justify-between">
-                    <button type="submit" class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-sm">Update</button>
-                    <Link :href="route('category.index')" class="inline-flex items-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 text-sm font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Back</Link>
+                    <button type="submit" :disabled="!form.isDirty && !props.category || form.processing" class="disabled:opacity-75 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-sm">{{!props.category ? 'Create' : 'Update'}}</button>
+                    <button type="button" v-if="!props.category" @click.prevent="handleCancel" class="inline-flex items-center rounded-md border border-transparent bg-rose-100 px-3 py-2 text-sm font-medium leading-4 text-rose-700 hover:bg-rose-200 focus:outline-none focus:ring-2 focus:ring-rose-500 focus:ring-offset-2">Cancel</button>
+                    <Link v-else :href="route('category.index')" class="inline-flex items-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 text-sm font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Back</Link>
                 </div>
+                <small v-if="form.isDirty && !props.category" class="mt-2 text-amber-400 dark:text-amber-200">There are unsaved form changes.</small>
             </form>
             </DialogPanel>
             </TransitionChild>
@@ -50,15 +54,20 @@
         </div>
      </template>
      <div class="py-8 px-8 mt-4 sm:px-2">
-         <div class="flex items-end space-x-2">
-            <h2 class="text-3xl font-bold text-gray-700 dark:text-indigo-600">Most Categories</h2>
-            <span class="bg-indigo-500 px-2 rounded-md text-sm font-semibold text-white">1 new</span>
-        </div>
+         <div class="flex justify-between mx-16">
+            <div class="flex items-end space-x-2">
+                <h2 class="text-3xl font-bold text-gray-700 dark:text-indigo-600">Most Categories</h2>
+                <span v-if="props.new_items != 0" class="bg-indigo-500 px-2 rounded-md text-sm font-semibold text-white">{{ props.new_items }} new</span>
+            </div>
+            <div class="mt-4">
+                <button @click.prevent="open = true" class="inline-flex items-center rounded-md border border-transparent bg-teal-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2">Create</button>
+            </div>
+         </div>
         <div class="grid grid-cols-2 md:grid-cols-4 md:px-12 md:gap-8 gap-4 mt-8">
             <div class="rounded-xl shadow-sm bg-white dark:bg-gray-800 pt-6 space-y-2" v-for="category in categories" :key="category.id">
                 <div class="flex justify-center items-end space-x-2">
                     <h1 class="text-center dark:text-gray-100 font-semibold">{{category.title}}</h1>
-                    <span v-show="category.items != 0" class="dark:text-white text-xs bg-indigo-500 px-2 rounded-md">{{ category.items }} New</span>
+                    <span v-show="category.items != 0" class="text-white text-xs bg-indigo-500 px-2 rounded-md">{{ category.items }} New</span>
                 </div>
                 <div class="flex justify-center">
                     <div>
@@ -68,7 +77,7 @@
                 </div>
                 <div class="flex justify-between p-2">
                     <Link :href="route('category.index', category.id)" class="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Edit</Link>
-                    <Link preserve-state :href="route('category.index', category.id)" class="inline-flex items-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 text-sm font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Delete</Link>
+                    <Link preserve-state :href="route('category.destroy', category.id)" as="button" method="DELETE" class="inline-flex items-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 text-sm font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Delete</Link>
                 </div>
             </div>
         </div>
@@ -78,14 +87,18 @@
 <script setup>
     import AppLayout from '@/Layouts/AppLayout.vue';
     import Breadcrumbs from '@/Components/Breadcrumbs.vue';
-    import { XCircleIcon } from '@heroicons/vue/24/outline';
+    import { XCircleIcon, TrashIcon } from '@heroicons/vue/24/outline';
     import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue'
     import InputError from '@/Components/InputError.vue';
     import InputLabel from '@/Components/InputLabel.vue';
     import TextInput from '@/Components/TextInput.vue';
     import { Link, useForm } from '@inertiajs/vue3';
     import { watchEffect,ref } from 'vue';
-    const props = defineProps({categories: Object, category: Object})
+    const props = defineProps({
+        categories: Object, 
+        category: Object, 
+        new_items: Number
+    })
     const form = useForm({
         title: ''
     });
@@ -95,8 +108,20 @@
         form.title = !props.category ? '' : props.category.title
     });
     const submit = () => {
-        form.put(route('category.update', props.category.id, {
-            preserveState: false,
-        }))
+        if(props.category != null){
+            form.put(route('category.update', props.category.id, {
+                preserveState: false,
+            }))
+        }else{
+            form.post(route('category.store'), {
+                preserveScroll: true,
+                onSuccess: () => form.reset('title')
+            })
+            open.value = false;
+        }
+    }
+    const handleCancel = () => {
+        form.cancel();
+        open.value = false
     }
 </script>
